28f51f2f7611666e3bd35cbd606033bacbb09cef
gadisom <jeongwon.kim@gmail.com>
2025-05-04 00:14:22 +0900

    ARC initial commit

---
+++ MemoryleakSwiftUI/MemoryleakSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryleakSwiftUI
+//
+//  Created by 김정원 on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryleakSwiftUI/MemoryleakSwiftUI/MemoryleakSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryleakSwiftUIApp.swift
+//  MemoryleakSwiftUI
+//
+//  Created by 김정원 on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryleakSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
